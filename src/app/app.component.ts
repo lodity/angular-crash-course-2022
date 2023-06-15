78d577b9ff37b78c491851ea0ead3950307d5ac2
lodity <lodity9@gmail.com>
2023-06-15 16:09:55 +0300

    added basic product cards and its output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crash-course-2022';
+  title = 'angular app';
+  products: IProduct[] = data;
 }
