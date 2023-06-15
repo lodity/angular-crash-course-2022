7c49644ad799075527fcf00b6328d964af1961c3
lodity <lodity9@gmail.com>
2023-06-15 16:58:03 +0300

    added products fetching

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ProductComponent, GlobalErrorComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
